fc50d2ce5b724fbe4bdf26ecbf14c9975d7a8e32
chrchung <cachungchristina@gmail.com>
2016-11-08 16:23:01 -0500

    Added demo with bacteria

---
+++ jackpotMachine.js
@@ -0,0 +1,19 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+
+    $scope.bacteria = [];
+
+
+    $scope.select = function (ob) {
+    	$('#' + ob.target.id).addClass('selected');
+    };
+
+
+	for (var i = 1; i < 9; i ++) {
+		$scope.bacteria.push([]);
+
+		for (var j = 1; j < 5; j ++) {
+			$scope.bacteria[i - 1].push({url: '/Users/Christina/Dropbox/MatchingGame/GameDesignIdeas/bacteriajackpot/' + i + j + '.png', id: ('' + i + j)});
+		}
+	}
+});
